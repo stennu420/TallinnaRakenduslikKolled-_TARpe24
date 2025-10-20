7eabcfb17f7977a8ac058d6c8f877b4d757a51f0
stennu420 <stenver.lindmaa@gmail.com>
2025-10-20 19:27:26 +0300

    nrmkor

---
+++ TallinnaRakenduslikKolledž/Properties/Controllers/DelinquentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TRK_TARpe24EN.Data;
+using TRK_TARpe24EN.Models;
+
+namespace TRK_TARpe24EN.Controllers
+{
+   public class DelinquentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Delinquents delinquent)
+        {
+             if (ModelState.IsValid)
+             {
+
+             }
+             return RedirectToAction("Index");
+        }
+    }
+}
